035f2b9544968d3ddb1b9bd81049692dfaa4851d
Kyle Olodin <kylemartinezolodin@gmail.com>
2022-05-11 22:12:18 +0800

    Updated /cats and /cats/<id> endpoints
    Changes to be committed:
    	modified:   server/src/app.py
    	new file:   server/src/v1/cat/controller.py
    	renamed:    server/src/v1/cat/cat.py -> server/src/v1/cat/model.py
    	new file:   server/src/v1/cat/router.py
    	new file:   server/src/v1/cat/test.http

--- server/src/app.py
+++ server/src/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, jsonify, request, make_response, g, Blueprint
+from flask import Flask, jsonify, request, make_response, g
+from flask import Blueprint
 from v1.dog.router import DogRouter
-# from models.dog import Dog
-from models.cat import Cat
+from v1.cat.router import CatRouter
 from db import Db
 
 app = Flask(__name__)
@@ -10,41 +10,9 @@ bp_dogs = Blueprint('dogs', __name__, url_prefix='/v1/dogs')
 DogRouter.handler(bp_dogs)
 app.register_blueprint(bp_dogs)
 
-@app.route('/cats', methods=['POST', 'GET', 'PUT', 'DELETE'])
-def cats():
-    cat = Cat()
-    if str(request.method).upper() == 'POST':
-        resp = cat.post(request.json)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to add. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'PUT':
-        return jsonify(cat.put(request.json))
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat.delete(request.json))
-    cats = cat.get()
-    return jsonify(cats)
-
-@app.route('/cats/<cat_id>', methods=['GET', 'PUT', 'DELETE'])
-def cat(cat_id):
-    cat_object = Cat()
-    cat = cat_object.get({"id": cat_id})
-    if cat is None:
-        return make_response(jsonify({"error": "Cat id not found."}), 404)
-    if str(request.method).upper() == 'PUT':
-        cat_data = request.json
-        cat_data['id'] = cat_id
-        resp = cat_object.put(cat_data)
-        if resp == False:
-            return make_response(jsonify({
-                "error": "Failed to update. There are items in your request that are invalid."
-            }), 400)
-        return jsonify(resp)
-    if str(request.method).upper() == 'DELETE':
-        return jsonify(cat_object.delete(cat_id))
-    return jsonify(cat)
+bp_cats = Blueprint('cats', __name__, url_prefix='/v1/cats')
+CatRouter.handler(bp_cats)
+app.register_blueprint(bp_cats)
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=6000)
